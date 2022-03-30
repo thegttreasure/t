776be2596f3f6b5890e9d4bbeb4661f3ef60b3ab
thecyberweapon <78067399+thecyberweapon@users.noreply.github.com>
2022-03-30 13:24:12 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -10,7 +10,7 @@ from pyrogram import idle
 
 from main.plugins.main import Bot, userbot
 
-st = "Oii Buddy ðŸ¤¡ __Send me Link of any message to clone it here, For private channel message, Send invite link first.__\n\nDEV: @TheCyberWeapon"
+st = "Hoi Buddy 🤖 __Send me Link of any message to clone it here, For private channel message, Send invite link first.__\n\nDEV: @TheCyberWeapon"
 
 @Worker.on(events.NewMessage(incoming=True, pattern="/start"))
 
